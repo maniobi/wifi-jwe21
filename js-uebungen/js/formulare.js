d073d3c0da7875dde93c58493cfbde3d51a365b4
Manuel Obermoser <mo@obinet.at>
2021-01-27 18:04:10 +0100

    dynamic form fields, char counter, animation, scroll position

--- js-uebungen/js/formulare.js
+++ js-uebungen/js/formulare.js
@@ -44,15 +44,129 @@ f_username.keyup(function(){
 
 });
 
+
+/*
+    dynamische Ausgabe von HTML (Formular Felder)
+*/
+
+/* 
+    Anzahl der Kinder 'children' 
+    und <div />, indem sich später die Felder für das Alter der jeweilien Kinder befinden 'children_ages' 
+    global verfügbare Variablen
+*/
+let f_children = $('#children');
+let f_children_ages = $('#children_ages');
+
+f_children.change(
+    function() {
+
+        // momentan ausgewählter Wert (Value) des Select-Feldes
+        let amount =  $(this).val();
+        console.log(amount);
+        
+        // Entfernen von (eventuell zuvor durch vorherige Auswahl eingefügtem) HTML
+        f_children_ages.html('');
+
+
+        // Schleife verwendet 'amount' aus dem Select-Feld um die Anzahl der Wiederholungen festzulegen
+        for( let i=0; i < amount; i++ ) {
+            console.log('Kind ' + (i+1) );
+            
+            /* 
+                Zusammenbauen einer Zeichenkette (String HTML-Code) für ein neues Feld
+                 => input-Variable wird mit jeder Zeile um HTML-Code erweitert
+                 => Template Schreibweise ${i+1} für das einfügen des Index
+                 => input = '<neuer HTML-Code />' + input (bisheriger Wert der input-Variable)
+                 => Anfügen an bestehendes HTML
+
+            */
+           
+            let input = `<input type="text" id="child_${i+1}_age" class="form-control child-age" required>`;
+            input = `<label for="child_${i+1}_age">Alter Kind ${i+1}:</label>` + input;
+
+            // Das bisherige HTML wird nun durch ein <div>-Kontrukt umschlossen
+            input = `<div class="row child">  <div class="col-md-3"> ` + input + ` </div>  </div> `;
+
+
+
+            /*
+                Ergebniss eines Durchgangs der Schleife sollte sein (am Beispiel ${i+1} = 1):
+
+                 <div class="row child">
+                    <div class="col-md-3">
+                      <label for="child_1_age">Alter Kind 1:</label>
+                      <input type="text" id="child_1_age" class="form-control" required>
+                    </div>
+                  </div> 
+            
+            */
+            // Anhängen des zusammengebauten HTML-Konstruktes
+            f_children_ages.append( input );
+        }
+    }
+);
+
+
+// Zugriff auf die <textarea /> 'message'
+let message = $('#message');
+
+// Zugriff auf den Counter-Tag (<strong id="counter" />)
+let counter = $('#counter');
+
+message.keyup(function() {
+    // wurde etwas vereinfacht (!)
+    // im Vergleich zu der gezeigten Variante wurde die Ausgabe direkt (ohne Variablendeklaration) geschrieben  
+    counter.html( $(this).val().length );
+});
+
+
 let f_submit = $('#checkoutSubmit');
+
+/* 
+    globale Definition nicht unbedingt notwendig.
+
+    Könnte auch innerhalb der 
+        f_submit.click(function(e) {
+            let f_children_ages_isValid;
+        });
+    definiert werden solange die Variable nirgendwo außerhalb benötigt wird.
+
+*/
+let f_children_ages_isValid;
+
 f_submit.click(function(e) {
 
-    // Treffen alle 3 Bedingungen zu dann ist das Feld "username" korrekt ausgefüllt
-    if(f_username_isValid == 3) {
+    // Zu Beginn des Events auf 0 setzen
+    f_children_ages_isValid = 0;
+    
+    $('input.child-age').each(function(){
+        
+        let field = $(this);
+
+        /* 
+            Der Inhalt (die Zeichenlänge) ist größer 0
+            => da Feld wurde befüllt, daher wird die Variable um 1 erhöht
+            => hier könnte noch eine explizite Prüfung für das Alter erfolgen            
+        */
+        if(field.val().length > 0) {
+            f_children_ages_isValid++;
+        }
+    });
+
+    /*
+        Ist die Zahl in 'f_children_ages_isValid' gleich hoch wie die Zahl in 'f_children.val()'
+        UND
+        f_username_isValid == 3
+        dann ist der gesamte Ausdruck: true
+        und das Formular wird per .submit() abgeschickt
+    */
+    if( f_children_ages_isValid == f_children.val() && f_username_isValid == 3) {
         $(this).closest('form').submit();
         return true;
     }
-    
+
+    // durch ein vorangestelltes return true; wird der nachstehende Code nicht mehr ausgeführt.
+
     e.preventDefault();
     return false;
 
---
+++ js-uebungen/js/uebung_jquery.js
@@ -0,0 +1,80 @@
+let btn = $('button');
+let box = $('#box');
+let boxIsVisible = false;
+let count = 1;
+
+btn.click(function() {
+
+    if(boxIsVisible == false) {
+        boxIsVisible = true;
+
+        box.stop().animate({
+            opacity: 1,
+            top: '-=100px',
+            width: '300px',
+            height: '300px',
+            fontSize: '200%'
+
+        }, 1000, 'easeOutExpo', function() {
+            console.log('Die Box wurde eingeblendet');
+        });
+
+        btn.html('Count up');
+
+    } else {
+
+        if (count > 99) {
+            count = 1;
+        }
+
+        box.html(count);
+
+        // Ansteigende Schriftgröße
+        box.css({
+            'font-size': 180 / ( 10 / count * 1.5 ) + '%',
+            'transition': 'font-size 1s ease'
+        });
+        count++;
+    }
+
+    
+});
+
+
+let scrollPostitionBasedAnimationStarted = false;
+let boxShape = 'square';
+
+$(window).scroll(function() {
+    
+    let scrollPos = $(document).scrollTop();
+
+    if(scrollPos > 300) {
+
+        if(scrollPostitionBasedAnimationStarted != true && boxShape == 'square') {
+            scrollPostitionBasedAnimationStarted = true;
+
+            box.stop().animate({
+                borderRadius: '50%'
+            }, 100, 'linear', function() {
+                console.log('Box-Transformation abgeschlossen.');
+                scrollPostitionBasedAnimationStarted = false;
+                boxShape = 'disc';
+            });
+
+        }
+    } else {
+        if(scrollPostitionBasedAnimationStarted != true && boxShape == 'disc') {
+            scrollPostitionBasedAnimationStarted = true;
+
+            box.stop().animate({
+                borderRadius: '0%'
+            }, 100, 'linear', function() {
+                console.log('Box-Transformation abgeschlossen.');
+                scrollPostitionBasedAnimationStarted = false;
+                boxShape = 'square';
+            });
+
+        }
+    }
+
+});
